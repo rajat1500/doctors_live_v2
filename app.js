c1927dfc3e7d4554cde2ce6d2b0ac5f0fb5891ae
rajat <rg688171@gmail.com>
2020-09-29 13:56:11 +0530

    addedfiles

--- app.js
+++ app.js
@@ -285,7 +285,13 @@ app.post('/register', function(req, res) {
 
 
 
-app.listen(3000, function() {
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
   console.log("server is running at 3000");
 })
 
